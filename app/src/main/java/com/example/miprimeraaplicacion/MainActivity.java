49b7aa97d63f8fa60c8ed090b61e8a8d9e865bd3
Daniel Zelaya <danilezelayavargas@gmail.com>
2025-02-18 09:31:08 -0600

    funcionamiendo de sensor de luz

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -16,6 +16,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView lblRespuesta;
