4905c93c9429b40fa77e7609f9924c78902e062b
Daniel Zelaya <danilezelayavargas@gmail.com>
2025-06-03 10:53:10 -0600

    Continuamos con el chats de amigos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.miprimeraaplicacion"
+    namespace = "com.ugb.miprimeraaplicacion"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.miprimeraaplicacion"
+        applicationId = "com.ugb.miprimeraaplicacion"
         minSdk = 23
         targetSdk = 35
         versionCode = 1
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
